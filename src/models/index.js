2f4b212c1a6b98a6b8c4cd48a1522246cc89ed4d
Kunal Kumar <kumarkunal8759@gmail.com>
2024-12-11 23:14:31 +0530

    try to fix the issue 2

--- src/models/index.js
+++ src/models/index.js
@@ -1,3 +1,52 @@
+// 'use strict';
+
+// require('dotenv').config(); // Load environment variables from .env file
+// const fs = require('fs');
+// const path = require('path');
+// const Sequelize = require('sequelize');
+// const basename = path.basename(__filename);
+// const db = {};
+
+// const { DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT, DB_DIALECT } = require('../config/serverConfig');
+
+
+
+// // Initialize Sequelize
+// let sequelize;
+// sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+//   host: DB_HOST,
+//   port: DB_PORT || 3306, // Default MySQL port is 3306
+//   dialect: DB_DIALECT || 'mysql', // This is the default dialect for MySQL
+//   logging: false // Disable logging; you can enable it for debugging
+// });
+
+// // Dynamically load all model files
+// fs.readdirSync(__dirname)
+//   .filter(file => {
+//     return (
+//       file.indexOf('.') !== 0 &&
+//       file !== basename &&
+//       file.slice(-3) === '.js' &&
+//       file.indexOf('.test.js') === -1
+//     );
+//   })
+//   .forEach(file => {
+//     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+//     db[model.name] = model;
+//   });
+
+// // Set up model associations
+// Object.keys(db).forEach(modelName => {
+//   if (db[modelName].associate) {
+//     db[modelName].associate(db);
+//   }
+// });
+
+// // Export Sequelize instance and models
+// db.sequelize = sequelize;
+// db.Sequelize = Sequelize;
+
+// module.exports = db;
 'use strict';
 
 require('dotenv').config(); // Load environment variables from .env file
@@ -7,20 +56,19 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const db = {};
 
+// Load database configuration from environment variables
 const { DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT, DB_DIALECT } = require('../config/serverConfig');
 
 
-
 // Initialize Sequelize
-let sequelize;
-sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
-  port: DB_PORT || 3306, // Default MySQL port is 3306
-  dialect: DB_DIALECT || 'mysql', // This is the default dialect for MySQL
-  logging: false // Disable logging; you can enable it for debugging
+  port: DB_PORT,
+  dialect: DB_DIALECT,
+  logging: false, // Disable logging; enable for debugging if needed
 });
 
-// Dynamically load all model files
+// Dynamically load all model files in the current directory
 fs.readdirSync(__dirname)
   .filter(file => {
     return (
